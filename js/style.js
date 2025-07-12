d02e9ecc689929bb89d4885ad29d5bd49b108cd0
huong-28 <108195927+huong-28@users.noreply.github.com>
2025-07-12 21:49:50 +0700

    [UP] first commit

---
+++ js/style.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+
+    AOS.init({disable: 'mobile'});
+
+    //Menu mobile toggle
+    $(".bar-icon, .close-icon").on("click", function() {
+      $("header nav").toggleClass("active");
+    })
+
+    new Swiper(".swiper-hero-banner", {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+
+    new Swiper(".swiper-about", {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+
+    new Swiper(".swiper-work", {
+      spaceBetween: 0,
+      loop: true,
+      breakpoints: {
+        0: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        480: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 40
+        }
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+
+    new Swiper(".swiper-news", {
+      slidesPerView: 2.5,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+});
+
