9a1e8df955ea0e1b73e255776f398957df7bedc4
huong-28 <108195927+huong-28@users.noreply.github.com>
2025-07-12 22:11:16 +0700

    space between our work section

--- js/style.js
+++ js/style.js
@@ -1,79 +1,77 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  AOS.init({ disable: "mobile" });
 
-    AOS.init({disable: 'mobile'});
+  //Menu mobile toggle
+  $(".bar-icon, .close-icon").on("click", function () {
+    $("header nav").toggleClass("active");
+  });
 
-    //Menu mobile toggle
-    $(".bar-icon, .close-icon").on("click", function() {
-      $("header nav").toggleClass("active");
-    })
+  new Swiper(".swiper-hero-banner", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
-    new Swiper(".swiper-hero-banner", {
-      slidesPerView: 1,
-      spaceBetween: 0,
-      loop: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
+  new Swiper(".swiper-about", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
-    new Swiper(".swiper-about", {
-      slidesPerView: 1,
-      spaceBetween: 0,
-      loop: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+  new Swiper(".swiper-work", {
+    spaceBetween: 0,
+    loop: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 2,
+        spaceBetween: 20,
       },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 20,
       },
-    });
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 0,
+      },
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
-    new Swiper(".swiper-work", {
-      spaceBetween: 0,
-      loop: true,
-      breakpoints: {
-        0: {
-          slidesPerView: 2,
-          spaceBetween: 20
-        },
-        480: {
-          slidesPerView: 2,
-          spaceBetween: 20
-        },
-        1024: {
-          slidesPerView: 4,
-          spaceBetween: 40
-        }
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
-
-    new Swiper(".swiper-news", {
-      slidesPerView: 2.5,
-      spaceBetween: 30,
-      loop: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
+  new Swiper(".swiper-news", {
+    slidesPerView: 2.5,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 });
-
